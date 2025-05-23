efb9a89a218bccf057e8fbbd33f7c4910df63fe5
AhmadR_R <105217847+Cxde02@users.noreply.github.com>
2025-05-23 20:05:30 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function genQR() {
-  var gapi = "https://chart.googleapis.com/chart?chf=bg,s,65432100&cht=qr&chs=";
+  var gapi = "";
   var myimg = document.getElementById("img");
   var mytext = document.getElementById("qrtext").value;
   var mysize = document.getElementById("size").value;
@@ -11,7 +11,6 @@ function genQR() {
     myimg.src = gapi + "100x100" + "&chl=" + mytext;
     document.getElementById("loadingText").innerText =
       "Please wait...Generating QR Code";
-    // https://chart.googleapis.com/chart?cht=qr&chs=100x100&chl=hello
   } else if (mytext !== "" && mysize == "150") {
     myimg.onload = function () {
       document.getElementById("loadingText").innerText = "";
